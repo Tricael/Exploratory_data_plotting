d8048d42431c206dd77ac3ef45c2f72bea7cd89a
Tricael <tyger.goose@gmail.com>
2016-04-23 21:35:08 -0400

    Ver2
    Error in the df name.

--- plot1.R
+++ plot1.R
@@ -5,7 +5,7 @@ library(lubridate)
 #setwd()   ##set your working dir
 plotdat<-read.table("household_power_consumption.txt",header=TRUE,sep=";",na.strings="?")
 plot3<-tbl_df(plotdat)
-plot3<-filter(plotdf,dmy(Date) >= ymd(20070201) & dmy(Date)<=ymd(20070202))
+plot3<-filter(plotdat,dmy(Date) >= ymd(20070201) & dmy(Date)<=ymd(20070202))
 plot3<-mutate(plot3,DateTime=paste(Date,Time))
 plot3$DateTime<-strptime(plot3$DateTime,"%d/%m/%Y %H:%M:%S")
 
